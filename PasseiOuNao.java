ece9273cfa8e5bfccc9d5d67aeae6a5ea200637a
Cindy Fagundes <144467488+cindyfagundes@users.noreply.github.com>
2023-09-10 00:11:49 -0300

    Create PasseiOuNao.java

---
+++ PasseiOuNao.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+
+public class PasseiOuNao {
+
+    public static void main(String[] args) {
+        float nota;
+        Scanner entrada = new Scanner (System.in); 
+            System.out.println("Digite sua nota [0.0 - 10.0]:");
+            nota=entrada.nextFloat();
+        
+        
+        if ((nota<=10.0)&& (nota>=0.0)){
+            
+            if (nota>=7.0){
+                System.out.println("Parabens, voce passou direto!");
+            }
+            else {
+                if(nota>=5.0){
+                System.out.println("Vai ter que fazer recuperacoa");
+                }
+                else {
+                System.out.println("Reprovado, ainda bem que e simulado, hein");
+            }
+        }
+    }
+         else {
+            System.out.println("Nota invalida, fechando aplicativo");
+             }
+    
+            }
+        }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import java.util.Scanner;
+
+public class PasseiOuNao {
+
+    public static void main (String[] args) {
+            float nota;
+         Scanner entrada = new Scanner (System.in);
+            
+
+            System.out.println("Digite sua nota (0.0 ate 10.0): ");
+            nota = entrada.nextFloat();
+
+            if ((nota>=0.0) && (nota<=10.0)) {
+                  
+                if (nota<5.0){
+                System.out.println("Reprovado direto");
+
+                }else  {
+                    if ((nota>=5.0) && (nota<7.0)){
+                    System.out.println("Esta de recuperacao");
+                    }else{
+                    System.out.println("Parabens! Voce passou direto");
+                    }
+                    }
+                
+                  } 
+            else{
+                System.out.println("Nota invalida! encerrando sistema");
+                }
+        }
+
+         }*/
+
+
+    
+
+
+
